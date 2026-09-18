e4e323a7038e10f7e811c11a1c88276f44be13f5
Dallin Romney <dallinromney@gmail.com>
2026-09-17 21:54:52 -0700

    test(telegram): remove rate waits from recovery fence

--- extensions/telegram/test-api.ts
+++ extensions/telegram/test-api.ts
@@ -1,2 +1,3 @@
+export { getOrCreateAccountThrottler } from "./src/account-throttler.js";
 export { renderTelegramProgressDraftPreview } from "./src/progress-draft-preview.js";
 export { telegramHtmlToPlainTextFallback } from "./src/format.js";
